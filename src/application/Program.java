842b2959cc8b742914f7c4d3346ba53acfc2220f
Andre Gomez <andregc1983@gmail.com>
2020-07-07 20:18:36 -0300

    Utilização do TreeSet para comparar elementos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,21 @@
 package application;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 import entities.Product;
 
 public class Program {
 	public static void main(String[] args) {
 	
-		Set<Product> set = new HashSet<>();
+		Set<Product> set = new TreeSet<>();
 		
 		set.add(new Product("TV", 900.0));
 		set.add(new Product("Notebook", 1200.0));
 		set.add(new Product("Tablet", 400.0));
-		
-		Product prod = new Product("Notebook", 1200.0);
-		
-		System.out.println(set.contains(prod)); //Sem a implementação de "HashCode" e "Equals" na classe Product, a comparação será feita pela referência do ponteiro do objeto na memória.
+	
+		for (Product p : set) {
+			System.out.println(p);
+		}
 	}
 }
